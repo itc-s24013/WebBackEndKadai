d245dd0384c01d853484455748eba986028fb57e
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-25 16:24:43 +0900

    DELETE /admin/author 完成

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -65,4 +65,35 @@ router.put('/author', async (req, res) => {
     }
 })
 
+router.delete('/author', async (req, res) => {
+    const author_id = req.body.id
+    if (!req.isAuthenticated()) {
+        return res.status(400).json({
+            reason: 'ログインをしてください'
+        })
+    }
+    if (req.user.is_admin !== true) {
+        return res.status(403).json({
+            reason: '管理者権限がありません'
+        })
+    }
+    try {
+        await prisma.author.update({
+            where: {
+                id: author_id
+            },
+            data: {
+                is_deleted: true
+            }
+        })
+        return res.status(200).json({
+            message: '著者情報を削除しました'
+        })
+    } catch (e) {
+        return res.status(400).json({
+            reason: e
+        })
+    }
+})
+
 export default router
